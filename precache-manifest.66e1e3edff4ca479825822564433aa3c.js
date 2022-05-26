368a038c7d8cdc3a09dafe46fbe93f0be9942eb8
Alfonzo <aamorales5b@gmail.com>
2022-05-26 17:32:07 -0400

    Updates

---
+++ precache-manifest.66e1e3edff4ca479825822564433aa3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "effb1a02a92b130e2ba3",
+    "url": "/Todo-App/static/css/main.5b02f152.chunk.css"
+  },
+  {
+    "revision": "effb1a02a92b130e2ba3",
+    "url": "/Todo-App/static/js/main.effb1a02.chunk.js"
+  },
+  {
+    "revision": "db389241ecef3d330313",
+    "url": "/Todo-App/static/js/1.db389241.chunk.js"
+  },
+  {
+    "revision": "58db14a1c0e0e44501c3",
+    "url": "/Todo-App/static/js/runtime~main.58db14a1.js"
+  },
+  {
+    "revision": "adcd16e5b3b902266b7f3c7cb148f9ed",
+    "url": "/Todo-App/index.html"
+  }
+];
